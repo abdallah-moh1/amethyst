38e5ca19c9703edce95beb75ff166fefc6dfbc22
Abdallah Mohammad <abdallah.moh.q@gmail.com>
2026-03-28 01:11:10 +0200

    Initialize project directory install react, electron, vite, and initial app dependencies

---
+++ electron/main.ts
@@ -0,0 +1,45 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        title: "Amethyst",
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.mjs'),
+            contextIsolation: true,
+            nodeIntegration: false,
+            sandbox: false
+        }
+    });
+
+    const devServerUrl = process.env.ELECTRON_START_URL;
+
+    if (devServerUrl) {
+        win.loadURL(devServerUrl);
+    } else {
+        win.loadFile(path.join(process.cwd(), 'dist', 'index.html'));
+    }
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+        <main className="app">
+            <h1>Amethyst</h1>
+            <p>{window.Amethyst.ping()}</p>
+        </main>
+    );
+}
