23c5c0983cf6d58ca7b402869c2f1c03aa2622d5
Abdallah Mohammad <abdallah.moh.q@gmail.com>
2026-03-28 18:49:58 +0200

    Add the SPDX License Identifier to all files

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Amethyst - A modern markdown note-taking application
+// Copyright (C) 2026 Abdallah
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
