e128fa08596275a194907e491e1d6f7694cc1653
Abdallah Mohammad <abdallah.moh.q@gmail.com>
2026-04-02 00:28:25 +0200

    Add the updated ipc api to the global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,3 +1,5 @@
+import { BuiltInThemes, Theme } from "@shared/types/themes.type";
+
 export { };
 
 declare global {
@@ -6,7 +8,13 @@ declare global {
             settings: {
                 get: (key: keyof Settings) => Promise<Settings[keyof Settings]>;
                 set: <K extends keyof Settings>(key: K, value: Settings[K]) => Promise<void>;
-            };
+                reset: () => Promise<void>;
+                getAll: () => Promise<Settings>;
+            },
+            themes: {
+                get: (key: BuiltInThemes) => Promise<Theme>,
+                list: () => Promise<Theme[]>,
+            },
         };
     }
 }
