47aa57b2d29b86786d3bd5e2a6f0ad2cb5097faf
Abdallah Mohammad <abdallah.moh.q@gmail.com>
2026-04-04 20:13:33 +0200

    chore: add prettier and eslint and add their corresponding scripts

---
+++ eslint.config.js
@@ -0,0 +1,51 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+    {
+        ignores: ['dist', 'dist-electron', 'node_modules'],
+    },
+
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+
+    {
+        files: ['src/**/*.{ts,tsx}'],
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+            globals: {
+                ...globals.browser,
+            },
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+        },
+    },
+
+    {
+        files: ['electron/**/*.ts', 'shared/**/*.ts', 'vite.config.ts'],
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+            globals: {
+                ...globals.node,
+            },
+        },
+    },
+
+    eslintConfigPrettier,
+);
