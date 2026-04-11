60989ca56252c568b373518a07b8192a59d6b369
Abdallah Mohammad <abdallah.moh.q@gmail.com>
2026-04-11 17:24:36 +0200

    feature: create needed types for notebook,note, facet

---
+++ shared/types/facet.type.ts
@@ -0,0 +1,9 @@
+import { FacetNote } from "./note.type.js";
+import { FacetNotebook } from "./notebook.type.js";
+
+export type Facet = {
+    path: string;
+    name: string;
+    notes: FacetNote[];
+    notebooks: FacetNotebook[];
+};
