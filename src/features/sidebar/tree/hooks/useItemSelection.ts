a7e606322ffcebc6262468d5d34062e6c76ad907
Abdallah Mohammad <abdallah.moh.q@gmail.com>
2026-04-13 16:15:23 +0200

    feature: load note content on click in the tree view

--- src/features/sidebar/tree/hooks/useItemSelection.ts
+++ src/features/sidebar/tree/hooks/useItemSelection.ts
@@ -1,30 +1,38 @@
-import { useFacetStore } from '@/store';
+import { openNote } from '@/clients/note.client';
+import { useFacetStore, useWorkspaceStore } from '@/store';
 import { useCallback } from 'react';
 import { TreeItemIndex } from 'react-complex-tree';
 
 export function useItemSelection() {
     const tree = useFacetStore((s) => s.tree);
     const selectedItem = useFacetStore((s) => s.selectedItem);
+
+
     const setSelectedItem = useFacetStore((s) => s.setSelectedItem);
+    const setNoteContent = useWorkspaceStore((s) => s.setNoteContent);
+
 
     const selectedItems: TreeItemIndex[] = selectedItem
         ? [selectedItem.type === 'note' ? selectedItem.id : selectedItem.path]
         : [];
 
     const handleSelectItems = useCallback(
-        (indices: TreeItemIndex[]) => {
+        async (indices: TreeItemIndex[]) => {
             const index = indices[0];
             const rctItem = tree[index];
             if (!rctItem) return;
             const data = rctItem.data;
             if (!data) return;
+            if (data.type === 'note') {
+                setNoteContent(await openNote(data.node.id));
+            }
             setSelectedItem(
                 data.type === 'notebook'
                     ? { type: 'notebook', path: data.node.path }
                     : { type: 'note', path: data.node.path, id: data.node.id },
             );
         },
-        [tree, setSelectedItem],
+        [tree, setSelectedItem, setNoteContent],
     );
     return {
         handleSelectItems,
