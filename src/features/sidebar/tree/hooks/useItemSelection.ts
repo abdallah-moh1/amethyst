e05f6675d7f22da36b4f2cd15caebe6405b8c042
Abdallah Mohammad <abdallah.moh.q@gmail.com>
2026-04-14 16:44:40 +0200

    fix: ensure usage of spdx license in all important files

--- src/features/sidebar/tree/hooks/useItemSelection.ts
+++ src/features/sidebar/tree/hooks/useItemSelection.ts
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Amethyst - A modern markdown note-taking application
+// Copyright (C) 2026 Abdallah
+
 import { openNote } from '@/clients/note.client';
 import { useFacetStore, useWorkspaceStore } from '@/store';
 import { useCallback } from 'react';
--- src/types/tree.type.ts
+++ src/types/tree.type.ts
@@ -1,9 +1,13 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Amethyst - A modern markdown note-taking application
+// Copyright (C) 2026 Abdallah
+
 import { FacetNote, FacetNotebook } from '@shared/types/facet.type';
 import { TreeItemIndex, TreeItem } from 'react-complex-tree';
 
 export type FacetTreeItemData =
-    | { type: 'note'; node: FacetNote }
-    | { type: 'notebook'; node: FacetNotebook };
+    | { type: 'note'; node: FacetNote; }
+    | { type: 'notebook'; node: FacetNotebook; };
 
 export type FacetTreeItem = TreeItem<FacetTreeItemData | null>;
 export type FacetTree = Record<TreeItemIndex, FacetTreeItem>;
