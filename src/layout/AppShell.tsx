8e3d6c3c58ad8d3057de72ece5e030b9f80a0646
Abdallah Mohammad <abdallah.moh.q@gmail.com>
2026-04-21 17:52:42 +0200

    fix: remove testing toast notifications

--- src/layout/AppShell.tsx
+++ src/layout/AppShell.tsx
@@ -2,35 +2,10 @@
 // Amethyst - A modern markdown note-taking application
 // Copyright (C) 2026 Abdallah
 
-import {
-    ToastMessage,
-    ToastNotifications,
-} from '@/features/toast-notifications/ToastNotifications';
+import { ToastNotifications } from '@/features/toast-notifications/ToastNotifications';
 import { WorkspacePanels } from './WorkspacePanels';
-import { useEffect } from 'react';
-import { useInteractionStore } from '@/store';
 
 export function AppShell() {
-    const addToast = useInteractionStore((s) => s.addToast);
-    useEffect(() => {
-        const interval = setInterval(() => {
-            const types: ToastMessage['type'][] = ['error', 'info', 'success', 'warning'];
-
-            const type = types[Math.floor(Math.random() * types.length)];
-
-            const newToast: ToastMessage = {
-                id: crypto.randomUUID(),
-                message: `Random message ${type} dddddddddddddddddddddddddddddddddddfss test long`,
-                duration: 4000,
-                type,
-            };
-
-            addToast(newToast);
-        }, 100000);
-
-        return () => clearInterval(interval);
-    }, []);
-
     return (
         <main className="app-shell">
             <WorkspacePanels />
