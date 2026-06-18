f496779f2da4feef18636078b7046b4835f30a66
Abdallah <abdallah.moh.q@gmail.com>
2026-06-18 19:30:49 +0300

    feat: allow dragging windows and have overlay titlebar for macos

--- src-tauri/src/windows.rs
+++ src-tauri/src/windows.rs
@@ -1,4 +1,7 @@
-use tauri::{AppHandle, Manager, WebviewUrl, WebviewWindow, WebviewWindowBuilder};
+use tauri::{
+    AppHandle, LogicalPosition, Manager, TitleBarStyle, WebviewUrl, WebviewWindow,
+    WebviewWindowBuilder,
+};
 
 pub const GEODES_MANAGER: &str = "geodes-manager";
 // pub const GEODE_WINDOW_PREFIX: &str = "geode-";
@@ -6,15 +9,30 @@ pub const GEODES_MANAGER: &str = "geodes-manager";
 pub fn create_geodes_manager_window(app: &AppHandle) -> tauri::Result<WebviewWindow> {
     if let Some(window) = app.get_webview_window(GEODES_MANAGER) {
         window.show()?;
+        window.unminimize()?;
         window.set_focus()?;
         return Ok(window);
     }
 
-    WebviewWindowBuilder::new(app, GEODES_MANAGER, WebviewUrl::App("index.html".into()))
-        .title("Amethyst")
-        .inner_size(600.0, 400.0)
-        .resizable(false)
-        .decorations(cfg!(target_os = "macos"))
-        .center()
-        .build()
+    let builder =
+        WebviewWindowBuilder::new(app, GEODES_MANAGER, WebviewUrl::App("index.html".into()))
+            .title("Amethyst")
+            .inner_size(600.0, 500.0)
+            .resizable(false)
+            .center();
+
+    #[cfg(target_os = "macos")]
+    let builder = builder.decorations(true);
+
+    #[cfg(not(target_os = "macos"))]
+    let builder = builder.decorations(false);
+
+    let window = builder.build()?;
+
+    #[cfg(target_os = "macos")]
+    {
+        window.set_title_bar_style(TitleBarStyle::Overlay)?;
+    }
+
+    Ok(window)
 }
