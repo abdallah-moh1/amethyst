b57366b74b79b8f29405aa246a7ddb41298ae62a
Abdallah <abdallah.moh.q@gmail.com>
2026-06-18 21:43:12 +0300

    fix: update lib name in main.rs to match the one in cargo.toml

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,10 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Amethyst - A modern markdown note-taking application
+// Copyright (C) 2026 Abdallah
+
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-  app_lib::run();
+    amethyst_lib::run();
 }
