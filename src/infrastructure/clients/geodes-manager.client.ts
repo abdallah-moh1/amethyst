622fb498a9a6a0dc3823cafff8d043ce2fafb9c8
Abdallah <abdallah.moh.q@gmail.com>
2026-06-23 02:49:45 +0300

    fix: fix the openGeodeMainWindow command wrapper

--- src/infrastructure/clients/geodes-manager.client.ts
+++ src/infrastructure/clients/geodes-manager.client.ts
@@ -13,11 +13,9 @@ import {
 import { invoke } from '@tauri-apps/api/core';
 
 export class GeodesManagerClient {
-    static async openGeodeMainWindow(id: GeodeIdInput) {
+    static async openGeodeMainWindow(input: GeodeIdInput) {
         return await invoke<void>('open_geode_main_window', {
-            input: {
-                id,
-            },
+            input,
         });
     }
 
